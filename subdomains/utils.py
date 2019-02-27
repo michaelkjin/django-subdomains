1218dd836180fda9d46592b80c1fcb947da9be9a
Michael Jin <mjin@healthsharedigital.com>
2019-02-28 10:49:53 +1100

    add more pdb traces

--- subdomains/utils.py
+++ subdomains/utils.py
@@ -1,3 +1,4 @@
+import pdb
 import functools
 try:
     from urlparse import urlunparse
@@ -51,6 +52,7 @@ def reverse(viewname, subdomain=None, scheme=None, args=None, kwargs=None,
     :param kwargs: named arguments used for URL reversing
     :param current_app: hint for the currently executing application
     """
+    pdb.set_trace()
     urlconf = settings.SUBDOMAIN_URLCONFS.get(subdomain, settings.ROOT_URLCONF)
 
     domain = get_domain()
